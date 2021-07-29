540977dd2b0ed9c366a2c5e6e97e606d3880ae00
tayoon <ta.tsu.1067@gmail.com>
2021-07-29 15:55:28 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-exports.handler = async () => {
+import { Client } from '@notionhq/client';
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export async function handler() {
   // TODO implement
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
   };
-  console.log('a');
+
+  const myPage = await notion.databases.query({
+    database_id: 'd2194450505742bea642149fb7aecab2',
+    filter: {
+      property: '作成日時',
+      text: {
+        equals: '2021-07-28',
+      },
+    },
+  });
+
+  console.log(myPage);
   return response;
-};
+}
