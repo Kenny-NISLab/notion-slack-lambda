c1777cffb882e013abe688d1b9e9b921edbf89e0
tayoon <ta.tsu.1067@gmail.com>
2021-07-29 17:03:28 +0900

    update

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const axios = require("axios");
+const { Client } = require("@notionhq/client");
 
-const config = {
-  headers: {
-    Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
-  },
-};
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
 
-exports.handler = () => {
+exports.handler = async function () {
   // TODO implement
   const response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda!"),
   };
 
-  axios.get("https://api.notion.com/v1/databases/d2194450505742bea642149fb7aecab2", config).then(function (response) {
-    console.log(response);
+  const myPage = await notion.databases.query({
+    database_id: "d2194450505742bea642149fb7aecab2",
+    filter: {
+      property: "作成日時",
+      text: {
+        equals: "2021-07-28",
+      },
+    },
   });
 
-  // const myPage = await notion.databases.query({
-  //   database_id: 'd2194450505742bea642149fb7aecab2',
-  //   filter: {
-  //     property: '作成日時',
-  //     text: {
-  //       equals: '2021-07-28',
-  //     },
-  //   },
-  // });
-
+  console.log(myPage);
   console.log("a");
   return response;
 };
