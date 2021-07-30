cfdacb638f03d68b2c5b0645f6190e828a3ae406
tayoon <ta.tsu.1067@gmail.com>
2021-07-30 11:18:48 +0900

    update

--- index.js
+++ index.js
@@ -36,24 +36,24 @@ const config = {
 
 exports.handler = async function () {
   // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify("Hello from Lambda!"),
+  // };
 
   const myPage = await notion.databases.query({
     database_id: "d2194450505742bea642149fb7aecab2",
     filter: {
       property: "作成日時",
       date: {
-        equals: "2021-07-23",
+        equals: yesterday,
       },
     },
   });
 
   const titleList = [];
   const results = myPage.results;
-  titleList.push("昨日作成された論文");
+  await titleList.push("昨日作成された論文");
   results.map((result) => {
     const tagList = [];
     const tags = result.properties["タグ"].multi_select;
@@ -72,20 +72,21 @@ exports.handler = async function () {
         ")"
       // "・【" + result.properties["作成者"].created_by.name + "】 <" + result.url + "|" + result.properties["論文名"].title[0].text.content + ">"
     );
-    console.log(result.properties["論文名"].title[0].text.content);
+    console.log("論文タイトル：", result.properties["論文名"].title[0].text.content);
+    console.log("Test a");
   });
 
-  config.data.text = titleList.join("\n");
+  config.data.text = await titleList.join("\n");
 
-  console.log(myPage.results[1].properties["論文名"].title[0].text.content);
+  // console.log(myPage.results[1].properties["論文名"].title[0].text.content);
 
   await axios(process.env.WEBHOOK_URL, config)
-    .then(function (response) {
-      console.log(response);
+    .then(function (res) {
+      console.log("axios res:", res);
     })
     .catch(function (error) {
       console.error(error);
     });
 
-  return response;
+  // return response;
 };
